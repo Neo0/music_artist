f8aeeabf48c922e6c4694e46ad9654c10f4c0b93
Neo0 <wjh100@live.cn>
2018-01-10 23:59:10 +0800

    添加音乐类目

--- crawler.py
+++ crawler.py
@@ -4,29 +4,43 @@ import urllib
 
 url = "https://freemidi.org/"
 
-result = requests.get("https://freemidi.org/genre-rock")
-c = result.content
+target = {
+    "https://freemidi.org/genre-pop",
+    "https://freemidi.org/genre-rock",
+    "https://freemidi.org/genre-classical",
+    "https://freemidi.org/genre-country",
+    "https://freemidi.org/genre-folk",
+    "https://freemidi.org/genre-jazz",
+    "https://freemidi.org/nationalanthems",
+    "https://freemidi.org/movies",
+    "https://freemidi.org/videogames"
+}
 
-soup = BeautifulSoup(c)
-samples = soup.find_all("div",class_="genre-link-text")
-href_list = []
-
-for s in samples:
-    a = s.a
-    href_list.append(a["href"])
-
-music_list = []
-
-for href in href_list:
-    result = requests.get(url+href)
+for url in target:
+    cat = url.rstrip("https://freemidi.org/")
+    result = requests.get(url)
     c = result.content
-    soup = BeautifulSoup(c)
-    for div in soup.find_all("div",class_="artist-song-cell"):
-        music = {"url":div.find("a", {"itemprop": "url"})["href"],"name":div.find("a", {"itemprop": "url"}).text.strip().replace(" ","_")}
-        music_list.append(music)
 
-for music in music_list:
-    music_href = music["url"]
-    tmp_list = music_href.split("-")
-    download_url = url + "getter-"+ tmp_list[1]
-    urllib.urlretrieve(download_url, "output/"+music["name"]+".midi")
+    soup = BeautifulSoup(c)
+    samples = soup.find_all("div",class_="genre-link-text")
+    href_list = []
+
+    for s in samples:
+        a = s.a
+        href_list.append(a["href"])
+
+    music_list = []
+
+    for href in href_list:
+        result = requests.get(url+href)
+        c = result.content
+        soup = BeautifulSoup(c)
+        for div in soup.find_all("div",class_="artist-song-cell"):
+            music = {"url":div.find("a", {"itemprop": "url"})["href"],"name":div.find("a", {"itemprop": "url"}).text.strip().replace(" ","_")}
+            music_list.append(music)
+
+    for music in music_list:
+        music_href = music["url"]
+        tmp_list = music_href.split("-")
+        download_url = url + "getter-"+ tmp_list[1]
+        urllib.urlretrieve(download_url, "output/"+cat+"/"+music["name"]+".midi")
